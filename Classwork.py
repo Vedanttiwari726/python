7095598e063bbfd93af87ded229b9a3e35bba5d8
Vedanttiwari726 <vedanttiwari986@gmail.com>
2024-07-01 17:27:40 +0530

    code for food ordering

---
+++ Classwork.py
@@ -0,0 +1,12 @@
+print("Breakfast,Lunch,Dinner")
+Meal=input("enter your meal:")
+if Meal=="Breakfast":
+    C=input("enter your order:")
+    print("your order is:",C)
+elif Meal=="Lunch":
+    C=input("enter your order:")
+    print("your order is:",C)
+elif Meal=="Dinner":
+    C=input("enter your order:")
+    print("your order is:",C)
+    
